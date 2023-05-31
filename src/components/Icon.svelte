f2ec4219e493af9d1ad7eab8665c29293ceca7b3
Sergey <m.sergeeva1983@mail.ru>
2023-05-31 14:36:36 +1000

    add todo app

---
+++ src/components/Icon.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let name;
+    const icons = [
+        {
+            name: 'check-mark',
+            path: 'static/favicon.png',     
+        },
+        {
+            name: 'delete',
+            path: 'none'
+        },
+    ];
+
+    let icon = icons.find((item) => item.name === name);
+</script>
+
