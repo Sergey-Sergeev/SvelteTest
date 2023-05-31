fe181509a54da69b79db260e08b4065c77f8a674
Sergey <m.sergeeva1983@mail.ru>
2023-05-31 10:58:34 +1000

    add main page and other

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<nav>
+    <dir class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Andrey Zorin</a>
+        <div class="nav-links">
+            <a href="/about" class="link">About me</a>
+            <a href="/blog" class="link">Blog</a>
+            <a href="/projects" class="link">My projects</a>
+            <a href="/contact" class="link">Contacts</a>
+        </div>
+    </dir>
+</nav>
+
+<div class="container">
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
